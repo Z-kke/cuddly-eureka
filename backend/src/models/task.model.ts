ffb229484e79e7a78cab4a3127449a6d6feeb91f
Saku Arho <sakurho@gmail.com>
2024-08-20 16:08:27 +0300

    Create a task model.

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,22 @@
+import { Table, Column, Model, DataType } from "sequelize-typescript";
+
+@Table
+export class Task extends Model {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title!: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: true,
+  })
+  description?: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  })
+  completed!: boolean;
+}
