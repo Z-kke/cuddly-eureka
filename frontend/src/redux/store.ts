caec854b71f0f0574b592154d2807a4a32576d47
Saku Arho <sakurho@gmail.com>
2024-08-26 18:28:40 +0300

    Update the redux store with taskReducer.

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "./taskSlice";
 
 const store = configureStore({
   reducer: {
-    // Reducers here
+    tasks: taskReducer,
   },
 });
 
